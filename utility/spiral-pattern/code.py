5bdd0930ab6ca69dbe6ff388268299bc38fa04c9
yoku2010 <yoku2010@gmail.com>
2016-09-05 01:23:46 +0530

    Spiral Patern Numbers

---
+++ utility/spiral-pattern/code.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+def spiral_pattern(number):
+    n = 2
+
+    while True:
+        if number <= n*n:
+            a = [["-" for x in range(n)] for y in range(n)]
+            break
+        n += 2
+
+    k = 0
+    l = n*n + 1
+
+    c1 = 0
+    c2 = n - 1
+
+    r1 = 0
+    r2 = n - 1
+
+    while k<n*n:
+        for i in range(c1, c2 + 1):
+            k+=1
+            a[r1][i] = l - k <= number and l - k or ""
+
+        for i in range(r1 + 1, r2 + 1):
+            k+=1
+            a[i][c2] = l - k <= number and l - k or ""
+
+        for i in range(c2-1, c1 - 1, -1):
+            k+=1
+            a[r2][i] = l - k <= number and l - k or ""
+
+        for i in range(r2-1, r1, -1):
+            k+=1
+            a[i][c1] = l - k <= number and l - k or ""
+
+        c1 += 1
+        c2 -= 1
+        r1 += 1
+        r2 -= 1
+
+    print_pattern(a, n);
+
+def print_pattern (a, n):
+    print "The Spiral Matrix is: "
+    print "\n".join(map(str, ["\t".join(map(str,a[i])) for i in range(n)]))
+
+if __name__ == "__main__":
+    number = int(raw_input("Enter Number of Element: "));
+    spiral_pattern(number);
